a7b44013692f3822e337069dd5eae334916ade92
aikokendo <alejandrameraz@gmail.com>
2020-12-18 20:52:37 -0500

    sol2 day11

--- 2020/classes/seatLayout.py
+++ 2020/classes/seatLayout.py
@@ -1,8 +1,11 @@
 class SeatLayout:
-    def __init__(self, state, tolerance, deep = False):
+    def __init__(self, state, tolerance, deep = False,noSeat = '.',emptySeat = 'L',occupiedSeat = '#'):
         self.state = state
         self.tolerance = tolerance
         self.deep = deep
+        self.noSeat = noSeat
+        self.emptySeat = emptySeat
+        self.occupiedSeat = occupiedSeat
 
     def changeState(self):
         w = len(self.state[0])
@@ -13,120 +16,53 @@ class SeatLayout:
             newRow = []
             for j in range(w):
                 item = self.state[i][j]
-                if item == '.':
-                    newRow.append('.')
-                if item == 'L':
+                if item == self.noSeat:
+                    newRow.append(self.noSeat)
+                if item == self.emptySeat:
                     itemsAround = self.getItemsAroundBasedOnDepth(i, j)
-                    if '#' not in itemsAround:
-                        newRow.append('#')
+                    if self.occupiedSeat not in itemsAround:
+                        newRow.append(self.occupiedSeat)
                     else:
-                        newRow.append('L')
-                if item == '#':
+                        newRow.append(self.emptySeat)
+                if item == self.occupiedSeat:
                     itemsAround = self.getItemsAroundBasedOnDepth(i, j)
-                    if itemsAround.count('#') >= self.tolerance:
-                        newRow.append('L')
+                    if itemsAround.count(self.occupiedSeat) >= self.tolerance:
+                        newRow.append(self.emptySeat)
                     else:
-                        newRow.append('#')
+                        newRow.append(self.occupiedSeat)
 
             newState.append(''.join(newRow))
         return newState
+
+    def findSeatInDirection(self,i,j,v,h):
+        width = len(self.state[0]) - 1
+        height = len(self.state) - 1
+        n = 1
+        while True:
+            newi = i + v * n
+            newj = j + h * n
+            if (newi >= 0) and (newi <= height) and newj >= 0 and newj <= width:
+                if self.state[newi][newj] in [self.emptySeat,self.occupiedSeat]:
+                    return self.state[newi][newj]
+            else:
+                return '/'
+            if not self.deep:
+                return '/'
+            n += 1
 
 
     def getItemsAroundBasedOnDepth(self, i, j):
         w = len(self.state[0])
         h = len(self.state)
         items = []
-        n = 1
-        while True:
-            if i - 1 * n >= 0:
-                if self.state[i - 1 * n][j] in ['L', '#']:
-                    items.append(self.state[i - 1 * n][j])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-        n = 1
-        while True:
-            if i - 1 * n >= 0 and j - 1 * n >= 0:
-                if self.state[i - 1 * n][j - 1 * n] in ['L', '#']:
-                    items.append(self.state[i - 1 * n][j - 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-        n = 1
-        while True:
-            if i - 1 * n >= 0 and j + 1 * n < w:
-                if self.state[i - 1 * n][j + 1 * n] in ['L', '#']:
-                    items.append(self.state[i - 1 * n][j + 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-
-        n = 1
-        while True:
-            if i + 1 * n < h:
-                if self.state[i + 1 * n][j] in ['L', '#']:
-                    items.append(self.state[i + 1 * n][j])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-        n = 1
-        while True:
-            if i + 1 * n < h and j - 1 * n >= 0:
-                if self.state[i + 1 * n][j - 1 * n] in ['L', '#']:
-                    items.append(self.state[i + 1 * n][j - 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-        n = 1
-        while True:
-            if i + 1 * n < h and j + 1 * n < w:
-                if self.state[i + 1 * n][j + 1 * n] in ['L', '#']:
-                    items.append(self.state[i + 1 * n][j + 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-
-        n = 1
-        while True:
-            if j - 1 * n >= 0:
-                if self.state[i][j - 1 * n] in ['L', '#']:
-                    items.append(self.state[i][j - 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-        n = 1
-        while True:
-            if j + 1 * n < w:
-                if self.state[i][j + 1 * n] in ['L', '#']:
-                    items.append(self.state[i][j + 1 * n])
-                    break
-            else:
-                break
-            if not self.deep:
-                break
-            n += 1
-
+        items.append(self.findSeatInDirection(i, j, -1, 0))
+        items.append(self.findSeatInDirection(i, j, -1, -1))
+        items.append(self.findSeatInDirection(i, j, -1, +1))
+        items.append(self.findSeatInDirection(i, j, +1, 0))
+        items.append(self.findSeatInDirection(i, j, +1, -1))
+        items.append(self.findSeatInDirection(i, j, +1, +1))
+        items.append(self.findSeatInDirection(i, j, 0, -1))
+        items.append(self.findSeatInDirection(i, j, 0, +1))
         return items
 
     def findEquilibrium(self):
