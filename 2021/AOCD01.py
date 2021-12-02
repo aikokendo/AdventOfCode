c13727ac4374c0b67121e92c55eb4a486b9a949b
aikokendo <alejandrameraz@gmail.com>
2021-12-01 19:40:41 -0500

    Day1 done

---
+++ 2021/AOCD01.py
@@ -0,0 +1,26 @@
+from classes.helper import Helper
+
+def depthVariance(input):
+    tot = 0
+    for i in range(1,len(input)):
+        if input[i] > input[i-1]:
+            tot += 1
+    return tot
+
+def depthVariance3Measures(input):
+    tot = 0
+    for i in range(3,len(input)):
+        if input[i-2] + input[i-1] + input[i] > input[i-3] + input[i-2] + input[i-1]:
+            tot += 1
+    return tot
+
+
+file_name = 'input\inputd01.txt'
+my_file_data = Helper.read_file(file_name)
+
+input = list(map(int,my_file_data.split('\n')))
+
+print('Part 1 solution:', depthVariance(input))
+print('Part 2 solution:', depthVariance3Measures(input))
+
+
